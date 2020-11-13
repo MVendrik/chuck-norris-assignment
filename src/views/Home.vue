6a13c0594665a80b2b583108bff71b7ddc64f6fb
MVendrik <35732059+MVendrik@users.noreply.github.com>
2020-11-13 14:43:52 +0100

    Adding functionality to fetch jokes and log them to the console.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <button class="joke-button" v-on:click="generateJokes">
+      Generate 10 jokes
+    </button>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "Home",
+  props: {},
+  methods: {
+    generateJokes: async function() {
+      let fetchedData = [];
+
+      let response = await fetch("http://api.icndb.com/jokes/random/10");
+
+      let parsed = await response.json();
+
+      fetchedData = parsed.value.map((item) => item.joke);
+
+      console.log(fetchedData);
+    },
+  },
+};
+</script>
