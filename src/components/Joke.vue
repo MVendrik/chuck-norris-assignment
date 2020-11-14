6babc31176692f84dd580f20d36946b074f7dd27
MVendrik <35732059+MVendrik@users.noreply.github.com>
2020-11-14 16:20:49 +0100

    I use localstorage to save favourites, since localstorage has no expiration date. For now, I use a simple onclick on a joke element to save it as a favourite. This can later be changed to a button. Favourites are saved in an array. Because localstorage can only save string values, I convert to JSON. In the favourites view the elements are read from localstorage and displayed.

--- src/components/Joke.vue
+++ src/components/Joke.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="joke">
-    <span>{{ text }}</span>
+    <span v-on:click="addToFavourites" ref="text">{{ text }}</span>
   </div>
 </template>
 
@@ -8,6 +8,25 @@
 export default {
   name: "Joke",
   props: ["text"],
+  methods: {
+    addToFavourites: function() {
+      let existingFavourites = JSON.parse(localStorage.getItem("favourites"));
+      console.log(typeof existingFavourites);
+
+      if (existingFavourites == null) {
+        existingFavourites = [];
+        console.log(existingFavourites);
+      }
+      let jokeToAdd = this.$refs.text.innerText;
+
+      existingFavourites.push(jokeToAdd);
+      console.log(existingFavourites);
+      window.localStorage.setItem(
+        "favourites",
+        JSON.stringify(existingFavourites)
+      );
+    },
+  },
 };
 </script>
 
--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="favourites">
     <h1>Favourites</h1>
+    <div ref="container"></div>
   </div>
 </template>
 
 <script>
+import Joke from "@/components/Joke.vue";
+import Vue from "vue";
+
 export default {
   name: "Favourites",
+  mounted() {
+    let favourites = JSON.parse(localStorage.getItem("favourites"));
+
+    for (let favourite of favourites) {
+      const ComponentClass = Vue.extend(Joke);
+      let instance = new ComponentClass({
+        propsData: { text: favourite },
+      });
+
+      instance.$mount();
+      this.$refs.container.appendChild(instance.$el);
+    }
+  },
 };
 </script>
 
